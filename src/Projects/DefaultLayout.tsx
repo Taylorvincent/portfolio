e2e6c233029dbf228b51bee3861a249032c300ec
Taylorvincent <vincent.sam.taylor@gmail.com>
2021-01-29 14:31:18 +0100

    Vimeo responsive embedd + add sass modules

--- src/Projects/DefaultLayout.tsx
+++ src/Projects/DefaultLayout.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { ProjectContent } from '../../data/projects'
+import styles from './styles.module.scss'
 
 interface Props {
 	content: ProjectContent
@@ -8,20 +9,16 @@ interface Props {
 const DefaultLayout = ({ content }: Props): JSX.Element => {
 	return (
 		<div className={`text-white  ${content.color}`}>
-			<div className={`text-xl p-4 mb-4 sticky top-0 ${content.color}`}>
+			<div className={`${content.color} border-b-2 md:border-b-0 text-xl p-4 mb-4 sticky top-0 `}>
 				<Link href="/">{`< Back`}</Link>
 			</div>
 			<div className="container mx-auto min-h-screen p-4">
 				<h1 className="text-7xl mb-8">{content.title}</h1>
 				<p className="text-4xl mb-16">{content.description}</p>
-
-				<div>
+				<div className={styles.embedContainer}>
 					<iframe
-						className="w-full"
 						title={'Video demo for ' + content.title + '.'}
 						src={content.video}
-						width="1344"
-						height="734"
 						frameBorder="0"
 						allowFullScreen
 					></iframe>
