6e89a2e48585511c4335297da0aa4b04865d5a04
Taylorvincent <vincent.sam.taylor@gmail.com>
2021-02-10 14:15:48 +0100

    add 404 route

---
+++ pages/projects/404.tsx
@@ -0,0 +1,13 @@
+import NotFound from '../../src/404'
+import Layout from '../../src/Layout'
+
+const SomeThingWentWrong = (): JSX.Element => {
+	return (
+		<Layout>
+			<link rel="stylesheet" type="text/css" href="/404/styles.css" media="screen" />
+			<NotFound></NotFound>
+		</Layout>
+	)
+}
+
+export default SomeThingWentWrong
