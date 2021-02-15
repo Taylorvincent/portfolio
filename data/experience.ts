09f5d70432d722a45b0bcade98d53a870ea6a367
Taylorvincent <vincent.sam.taylor@gmail.com>
2021-02-15 04:28:27 +0100

    update content experience

--- data/experience.ts
+++ data/experience.ts
@@ -2,24 +2,31 @@ const experience = [
 	{
 		company: {
 			name: '3D Aim Trainer',
-			desc: 'An online training platform for gamers.',
+			desc: 'Online training platform for gamers.',
 			url: 'https://www.3daimtrainer.com',
 		},
-		duration: 'Jan 2020 - Jan 2021',
+		duration: 'Jan. 2020 - Jan. 2021',
 		role: 'Front-end developer',
-		stack: 'next.js, react, typescript, styled-components, webpack, sentry, laravel',
-		content: ['Joined this startup to build their new front-end from scratch.'],
+		stack: 'next.js, react (hooks), typescript, styled-components, webpack, sentry, Laravel',
+		content: [
+			'Joined this start-up to transform their POC website to a mature platform with over 700,000 monthly users.',
+			'Took the lead in technical decisions of the front-end department. Always with performance, UX and DX in mind.',
+			'Implemented a component-based design system to keep UI consistency high.',
+		],
 	},
 	{
 		company: {
 			name: 'Sigura',
-			desc: 'A CRM tool for brokers.',
+			desc: 'CRM for brokers.',
 			url: 'https://www.sigura.be',
 		},
 		role: 'Full-stack developer',
-		duration: '1 year',
+		duration: 'Oct. 2018 – Jan. 2020',
 		stack: 'php, react, webpack, jquery, css',
-		content: ['Helped maintain legacy jquery/php codebase, updated parts to React when necessary.'],
+		content: [
+			'Helped maintain legacy jQuery/php codebase. Extended it with new features and updated parts to React when necessary.',
+			'Provided customer support for users (database querying).',
+		],
 	},
 	{
 		company: {
@@ -28,31 +35,44 @@ const experience = [
 			url: '/projects/visualizr',
 		},
 		role: 'Full-stack developer',
-		duration: '3 months',
-		stack: 'react, redux, express (node.js), mongoose (mongodb)',
-		content: ['Took a break to work on my own project, a browser based music Visualizr.'],
+		duration: 'Winter 2017 - 3 months',
+		stack: 'Three.js, react, redux, express (node.js), mongoose (mongodb)',
+		content: [
+			'Took a break to get creative and extend my thesis project, a browser-based music Visualizr. Tried to convert our POC into a working product.',
+			'Rewrote the user portal section of the app in react/redux with an express/mongodb back end.',
+			'Extended the music visualizer to be more composable, allowing new "visual show presets" to be easily created.',
+			'Added various small features like shaders, 3d models or fine tuning audio analysis.',
+		],
 	},
 	{
 		company: {
 			name: 'BBDO',
-			desc: 'Internship + summer job in this Brussels based advertising agency',
+			desc: 'Advertising agency',
 			url: 'https://www.bbdo.be',
 		},
 		role: 'Full-stack developer',
-		duration: '6 months',
-		stack: 'laravel, scss, gulp',
-		content: ['Created laravel websites for various clients.'],
+		duration: 'Spring & summer 2017 - 6 months',
+		stack: 'Laravel, scss, gulp',
+		content: [
+			'Back-end focused internship + summer job in this Brussels based agency.',
+			'Created Laravel websites for an intranet and various externals clients using a custom in-house CMS.',
+			'When not working for clients, I helped to extend the custom CMS with new features.',
+		],
 	},
 	{
 		company: {
 			name: 'The Fridge',
-			desc: 'Summer job in this Brussels based post-production facility',
+			desc: 'in this Brussels based post-production facility',
 			url: 'https://www.thefridge.tv/',
 		},
 		role: 'Full-stack developer',
-		duration: '3 months',
-		stack: 'django, flask, css',
-		content: ['Created a new website for a client.'],
+		duration: 'Summer 2016 - 3 months',
+		stack: 'python, django, flask, css',
+		content: [
+			'Summer job',
+			'Study a short online course on Python on the job. Then create a small CRUD Django app.',
+			'After getting up to speed with Python, I developed the front-end of a new website for a client.',
+		],
 	},
 ]
 
