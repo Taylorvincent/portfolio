2165b2ce8784fb553c3b556b0f6e265e31c23921
Vincent Taylor <vincent@wegroup.be>
2025-08-06 15:08:53 +0200

    simplify project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,8 @@
 module.exports = {
-	extends: ['next/core-web-vitals', 'prettier'],
-	plugins: ['@typescript-eslint'],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	env: {
-		browser: true,
-		es6: true,
-		node: true,
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+	extends: ['next/core-web-vitals'],
+	plugins: ['prettier'],
 	rules: {
-		'@typescript-eslint/explicit-function-return-type': [
-			'warn',
-			{
-				allowExpressions: true,
-				allowConciseArrowFunctionExpressionsStartingWithVoid: true,
-			},
-		],
-		'@typescript-eslint/no-unused-vars': ['error'],
-		'react/prop-types': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'jsx-a11y/anchor-is-valid': 'off',
+		'prettier/prettier': 'error',
 		'react/no-unescaped-entities': 'off',
 	},
 }
